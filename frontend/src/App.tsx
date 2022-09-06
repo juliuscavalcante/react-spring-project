1dc151c4544db5838e58fb39715e1e9396d43e43
Julius Cavalcante <julius.c@outlook.com.br>
2022-09-06 09:16:10 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,20 @@
-import NotificationButton from "./componentes/NotificationButton";
+import Header from "./componentes/Header";
+import SalesCard from "./componentes/SalesCard";
+
 
 function App() {
     return(
         <>
-            <h1>Olá mundo!</h1>
-            <NotificationButton/>
-            <NotificationButton/>
-            <NotificationButton/>
-            <NotificationButton/>
-            <NotificationButton/>
-            <NotificationButton/>
-            <NotificationButton/>
+            <Header />
+            <main>
+                <section id="sales">
+                    <div className="dsmeta-container">
+                        <SalesCard />
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
-
 
 export default App
